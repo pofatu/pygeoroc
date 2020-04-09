0bad8347cd362d25d9672fdde54b0da06dcf4db9
xrotwang <xrotwang@googlemail.com>
2020-04-09 16:08:16 +0200

    release 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygeoroc',
-    version='0.1.1.dev0',
+    version='0.2.0',
     license='Apache 2.0',
     description='programmatic access to GEOROC data',
     long_description=open('README.md').read(),
