0a2cc9a8baf27b115f31142defcd677e7b65ef1d
Robert Forkel <xrotwang@googlemail.com>
2021-03-10 13:32:58 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygeoroc',
-    version='1.0.0',
+    version='1.0.1.dev0',
     license='Apache 2.0',
     description='programmatic access to GEOROC data',
     long_description=open('README.md').read(),
--- src/pygeoroc/__init__.py
+++ src/pygeoroc/__init__.py
@@ -1,3 +1,3 @@
 from pygeoroc.api import GEOROC  # noqa: F401
 
-__version__ = '1.0.0'
+__version__ = '1.0.1.dev0'
